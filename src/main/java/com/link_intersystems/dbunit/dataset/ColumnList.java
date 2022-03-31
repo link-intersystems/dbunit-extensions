cbde5e3a977ea622921d023595f6fef37f0624ec
René Link <rene.link@link-intersystems.com>
2022-03-31 15:14:07 +0200

    Added ColumnList.

---
+++ src/main/java/com/link_intersystems/dbunit/dataset/ColumnList.java
@@ -0,0 +1,34 @@
+package com.link_intersystems.dbunit.dataset;
+
+import org.dbunit.dataset.Column;
+
+import java.util.AbstractList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ColumnList extends AbstractList<Column> {
+
+    private List<Column> columns;
+
+    public ColumnList(Column[] columns) {
+        this(Arrays.asList(columns));
+    }
+
+    public ColumnList(List<Column> columns) {
+        this.columns = columns;
+    }
+
+    public Column getColumn(String name) {
+        return stream().filter(c -> c.getColumnName().equals(name)).findFirst().orElse(null);
+    }
+
+    @Override
+    public Column get(int index) {
+        return columns.get(index);
+    }
+
+    @Override
+    public int size() {
+        return columns.size();
+    }
+}
