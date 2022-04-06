a60de7bf1588e489dc4184a08b19213b275b5a3a
René Link <rene.link@link-intersystems.com>
2022-04-06 08:43:24 +0200

    dbunit package javadoc.

---
+++ dbunit-core-extensions/src/main/java/com/link_intersystems/dbunit/dataset/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * DBUnit dataset extensions.
+ *
+ * @author René Link {@literal <rene.link@link-intersystems.com>}
+ */
+package com.link_intersystems.dbunit.dataset;
---
+++ dbunit-core-extensions/src/main/java/com/link_intersystems/dbunit/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Core DBUnit extension classes.
+ *
+ * @author René Link {@literal <rene.link@link-intersystems.com>}
+ */
+package com.link_intersystems.dbunit;
