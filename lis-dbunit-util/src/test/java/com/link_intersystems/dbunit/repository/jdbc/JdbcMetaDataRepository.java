4f438d979d55c664ed663c22be4032881d9257a6
René Link <rene.link@link-intersystems.com>
2022-05-11 09:26:15 +0200

    Added jdbc meta data support.

---
+++ lis-dbunit-util/src/test/java/com/link_intersystems/dbunit/repository/jdbc/JdbcMetaDataRepository.java
@@ -0,0 +1,110 @@
+package com.link_intersystems.dbunit.repository.jdbc;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @author René Link {@literal <rene.link@link-intersystems.com>}
+ */
+public class JdbcMetaDataRepository {
+
+    private List<JdbcTableMetaData> tableMetaDataList;
+    private Map<String, List<JdbcColumnMetaData>> columnMetaDataListByTableName = new HashMap<>();
+
+    private Connection connection;
+    private String[] tableTypes;
+
+
+    public JdbcMetaDataRepository(Connection connection) {
+        this(connection, new String[]{"TABLE"});
+    }
+
+    public JdbcMetaDataRepository(Connection connection, String[] tableTypes) {
+        this.connection = connection;
+        this.tableTypes = tableTypes;
+    }
+
+    public List<JdbcTableMetaData> getTableMetaDataList() throws SQLException {
+        if (tableMetaDataList == null) {
+            ScopedDatabaseMetaData metaData = getScopedDatabaseMetaData();
+            ResultSet tablesResultSet = metaData.getTables("%", tableTypes);
+            tableMetaDataList = mapResultSet(tablesResultSet, JdbcTableMetaData::new);
+        }
+        return tableMetaDataList;
+    }
+
+    private ScopedDatabaseMetaData getScopedDatabaseMetaData() throws SQLException {
+        DatabaseMetaData metaData = connection.getMetaData();
+        String catalog = connection.getCatalog();
+        String schema = connection.getSchema();
+        return new ScopedDatabaseMetaData(metaData, catalog, schema);
+    }
+
+    public JdbcTableMetaData getTableMetaData(String tableName) throws SQLException {
+        return getTableMetaDataList().stream().filter(jtmd -> jtmd.getTableName().equals(tableName)).findFirst().orElse(null);
+    }
+
+    public List<JdbcColumnMetaData> getColumnMetaDataList(JdbcTableMetaData jdbcTableMetaData) throws SQLException {
+        return getColumnMetaDataList(jdbcTableMetaData.getTableName());
+    }
+
+    public List<JdbcColumnMetaData> getColumnMetaDataList(String tableName) throws SQLException {
+        if (!columnMetaDataListByTableName.containsKey(tableName)) {
+            List<JdbcColumnMetaData> columnMetaDatas = createColumnMetaData(tableName);
+            columnMetaDataListByTableName.put(tableName, columnMetaDatas);
+        }
+        return columnMetaDataListByTableName.get(tableName);
+
+    }
+
+    private List<JdbcColumnMetaData> createColumnMetaData(String tableName) throws SQLException {
+        ScopedDatabaseMetaData scopedDatabaseMetaData = getScopedDatabaseMetaData();
+        ResultSet columnsMetaDataResultSet = scopedDatabaseMetaData.getColumns(tableName);
+
+        return mapResultSet(columnsMetaDataResultSet, JdbcColumnMetaData::new);
+    }
+
+    public List<JdbcForeignKey> getExportedKeys(String tableName) throws SQLException {
+        ScopedDatabaseMetaData scopedDatabaseMetaData = getScopedDatabaseMetaData();
+        ResultSet resultSet = scopedDatabaseMetaData.getExportedKeys(tableName);
+        List<JdbcForeignKeyEntry> jdbcForeignKeyEntries = mapResultSet(resultSet, JdbcForeignKeyEntry::new);
+
+        return mapToForeignKeys(jdbcForeignKeyEntries);
+    }
+
+    private List<JdbcForeignKey> mapToForeignKeys(List<JdbcForeignKeyEntry> jdbcForeignKeyEntries) {
+        Map<String, List<JdbcForeignKeyEntry>> foreignKeys = jdbcForeignKeyEntries.stream().collect(Collectors.groupingBy(JdbcForeignKeyEntry::getFkName));
+        return foreignKeys.values().stream().map(JdbcForeignKey::new).collect(Collectors.toList());
+    }
+
+    public List<JdbcForeignKey> getImportedKeys(String tableName) throws SQLException {
+        ScopedDatabaseMetaData scopedDatabaseMetaData = getScopedDatabaseMetaData();
+        ResultSet resultSet = scopedDatabaseMetaData.getImportedKeys(tableName);
+        List<JdbcForeignKeyEntry> jdbcForeignKeyEntries = mapResultSet(resultSet, JdbcForeignKeyEntry::new);
+        return mapToForeignKeys(jdbcForeignKeyEntries);
+    }
+
+    @FunctionalInterface
+    private static interface ElementFactory<T> {
+
+        public T apply(ResultSet resultSet) throws SQLException;
+    }
+
+    private <T> List<T> mapResultSet(ResultSet resultSet, ElementFactory<T> elementFactory) throws SQLException {
+        List<T> mappedResultSet = new ArrayList<>();
+
+        while (resultSet.next()) {
+            T element = elementFactory.apply(resultSet);
+            mappedResultSet.add(element);
+        }
+
+        return mappedResultSet;
+    }
+}
