35c7a206a5f6434acec5d0f7264fdf996e12f938
René Link <rene.link@link-intersystems.com>
2022-07-27 11:29:16 +0200

    Don't fail with file detection if apache poi is not on the classpath.

--- lis-dbunit-stream/src/main/java/com/link_intersystems/dbunit/stream/resource/file/xls/XlsDataSetDetector.java
+++ lis-dbunit-stream/src/main/java/com/link_intersystems/dbunit/stream/resource/file/xls/XlsDataSetDetector.java
@@ -2,13 +2,12 @@ package com.link_intersystems.dbunit.stream.resource.file.xls;
 
 import com.link_intersystems.dbunit.stream.resource.file.DataSetFile;
 import com.link_intersystems.dbunit.stream.resource.file.DataSetFileDetector;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
-import java.text.MessageFormat;
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * @author René Link {@literal <rene.link@link-intersystems.com>}
@@ -17,29 +16,35 @@ public class XlsDataSetDetector implements DataSetFileDetector {
 
     private Logger logger = LoggerFactory.getLogger(XlsDataSetDetector.class);
 
-    @Override
-    public DataSetFile detect(File file) {
-        if (file.isDirectory()) {
-            return null;
-        }
+    private DataSetFileDetector implementor;
 
-        if (file.getName().endsWith(".xls")) {
-            try (InputStream in = new BufferedInputStream(new FileInputStream(file))) {
-                Workbook workbook = WorkbookFactory.create(in);
-                workbook.close();
-                return new XlsDataSetFile(file);
-            } catch (IOException e) {
-                logFileNotReadable(file, e);
+    public XlsDataSetDetector() {
+        try {
+            implementor = createXlsDetectorImplementor();
+        } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            String msg = "XlsFileDetector is out of order. Maybe the apache poi library is not on the classpath.";
+            if (logger.isDebugEnabled()) {
+                logger.warn(msg, e);
+            } else {
+                logger.warn(msg);
             }
         }
+    }
 
+    @Override
+    public DataSetFile detect(File file) {
+        if (implementor != null) {
+            return implementor.detect(file);
+        }
         return null;
     }
 
-    private void logFileNotReadable(File file, IOException e) {
-        if (logger.isDebugEnabled()) {
-            String msg = MessageFormat.format("File ''{}'' with extension xls, doesn't seem to be an excel file.", file);
-            logger.debug(msg, e);
-        }
+    private DataSetFileDetector createXlsDetectorImplementor() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class.forName("org.apache.poi.ss.usermodel.Workbook");
+        Class<?> implementor = Class.forName("com.link_intersystems.dbunit.stream.resource.file.xls.XlsDataSetDetectorImplementor");
+        Constructor<?> declaredConstructor = implementor.getDeclaredConstructor();
+        return (DataSetFileDetector) declaredConstructor.newInstance();
     }
+
+
 }
