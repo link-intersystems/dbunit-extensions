67e05865a2454a8ddfb33c2938f601d446b893e5
René Link <rene.link@link-intersystems.com>
2022-08-27 04:54:19 +0200

    Added ConsistentDataSetLoaderDocTest.

---
+++ lis-dbunit-dataset/src/test/java/com/link_intersystems/dbunit/dataset/consistency/ConsistentDataSetLoaderDocTest.java
@@ -0,0 +1,54 @@
+package com.link_intersystems.dbunit.dataset.consistency;
+
+import com.link_intersystems.dbunit.table.TableUtil;
+import com.link_intersystems.jdbc.test.db.sakila.SakilaTinyExtension;
+import com.link_intersystems.test.ComponentTest;
+import org.dbunit.DatabaseUnitException;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author René Link {@literal <rene.link@link-intersystems.com>}
+ */
+@SakilaTinyExtension
+public class ConsistentDataSetLoaderDocTest {
+
+    @Test
+    void consistentLoad(Connection sakilaConnection) throws DatabaseUnitException {
+        ConsistentDataSetLoader dataSetLoader = new ConsistentDataSetLoader(new DatabaseConnection(sakilaConnection));
+
+        IDataSet dataSet = dataSetLoader.load("SELECT * from film_actor where film_actor.film_id = ?", 1);
+
+        String[] tableNames = dataSet.getTableNames();
+        assertArrayEquals(new String[]{"film_actor", "film", "actor", "language"}, tableNames);
+
+        ITable filmActorTable = dataSet.getTable("film_actor");
+        assertEquals(1, filmActorTable.getRowCount(), "film_actor entity count");
+        TableUtil filmActorUtil = new TableUtil(filmActorTable);
+
+        assertNotNull(filmActorUtil.getRowById(1, 1));
+
+        ITable actorTable = dataSet.getTable("actor");
+        assertEquals(1, actorTable.getRowCount(), "actor entity count");
+        TableUtil actorUtil = new TableUtil(actorTable);
+        assertNotNull(actorUtil.getRowById(1));
+
+        ITable filmTable = dataSet.getTable("film");
+        assertEquals(1, filmTable.getRowCount(), "film entity count");
+        TableUtil filmUtil = new TableUtil(filmTable);
+        assertNotNull(filmUtil.getRowById(1));
+
+        ITable languageTable = dataSet.getTable("language");
+        assertEquals(1, languageTable.getRowCount(), "language entity count");
+        TableUtil languageUtil = new TableUtil(languageTable);
+        assertNotNull(languageUtil.getRowById(1));
+    }
+}
+
