b1c68d6ef14174b484e4650f85bc33dc92c4b4a8
René Link <rene.link@link-intersystems.com>
2022-09-06 11:55:24 +0200

    Removed TableLiteralFormatFactory.java and added javadoc to TableLiteralFormatResolver.

--- lis-dbunit-sql/src/main/java/com/link_intersystems/dbunit/sql/consumer/TableLiteralFormatResolver.java
+++ lis-dbunit-sql/src/main/java/com/link_intersystems/dbunit/sql/consumer/TableLiteralFormatResolver.java
@@ -8,5 +8,11 @@ import org.dbunit.dataset.ITableMetaData;
  * @author René Link {@literal <rene.link@link-intersystems.com>}
  */
 public interface TableLiteralFormatResolver {
+    /**
+     * @param tableMetaData
+     * @return
+     * @throws DataSetException
+     * @see <a href="https://github.com/link-intersystems/lis-commons/blob/master/lis-commons-sql/src/main/java/com/link_intersystems/sql/statement/TableLiteralFormat.java">TableLiteralFormat</a>
+     */
     TableLiteralFormat getTableLiteralFormat(ITableMetaData tableMetaData) throws DataSetException;
 }
--- lis-dbunit-sql/src/main/java/com/link_intersystems/dbunit/sql/statement/TableLiteralFormatFactory.java
+++
@@ -1,7 +0,0 @@
-package com.link_intersystems.dbunit.sql.statement;
-
-/**
- * @author René Link {@literal <rene.link@link-intersystems.com>}
- */
-public interface TableLiteralFormatFactory {
-}
